24c4a2ace0335dcc911a7aa1e7fd8f21421531fe
BlackEdder <edder@tkwsping.nl>
2014-03-06 17:34:26 +0000

    Implemented the Gillespie algorithm

--- src/stochastic/gillespie.d
+++ src/stochastic/gillespie.d
@@ -27,18 +27,164 @@
 module stochastic.gillespie;
 
 import std.random;
+import std.container;
 
-interface Event
+interface RateMonitor
 {
-	real get_rate();
+	void update_rate( real old_rate, real new_rate );
 }
 
+/**
+	* BaseEvent class for the gillespie algorithm
+	*/
+class BaseEvent
+{
+	/// Get the current rate
+	@property auto rate() {
+		return myrate;
+	}
+
+	/// Changes the current rate
+	@property void rate( real new_rate ) {
+		if (mymonitor)
+			mymonitor.update_rate( rate, new_rate );
+		myrate = new_rate;
+	}
+
+	unittest {
+		BaseEvent ev = new BaseEvent;
+		assert( ev.rate == 0 );
+		ev.rate( 10 );
+		assert( ev.rate() == 10 ); 
+	}
+
+	/**
+		* Add a monitor that will be notified when the rate of the Event changes
+		*/
+	@property void monitor( RateMonitor rm ) {
+		mymonitor = rm;
+	}
+
+	unittest {
+		class MyMonitor : RateMonitor {
+			double latest_rate;
+			double old_rate;
+
+			void update_rate( real rate, real new_rate ) {
+				old_rate = rate;
+				latest_rate = new_rate;
+			}
+		}
+
+		BaseEvent ev = new BaseEvent;
+		MyMonitor m = new MyMonitor;
+		ev.monitor = cast(RateMonitor) m;
+		ev.rate( 10 );
+		assert( ev.rate == 10 );
+		assert( m.latest_rate == 10 ); 
+		ev.rate( 5 );
+		assert( m.old_rate == 10 ); 
+		assert( m.latest_rate == 5 );
+	}
+
+
+	private: 
+		real myrate = 0;
+		RateMonitor mymonitor = null;
+}
+
+/**
+	* Interface for EventContainer
+	*
+	* Currently EventList is the only implementation of this interface
+	*/
 interface EventContainer
 {
-	real get_total_rate();
-	void add_event( const Event );
-	void del_event( const Event );
+	@property real total_rate();
+	void add_event( BaseEvent );
 
-	real time_till_next_event();
-	Event random_event( ref Random gen );
+	/// Get time till next event
+	final real time_till_next_event( ref Random gen ) {
+		return stochastic.random.exponential( total_rate(), gen );
+	}
+
+	BaseEvent get_next_event( ref Random gen );
+}
+
+/**
+	* Main class used for the gillespie algorithm
+	*/
+class EventList : EventContainer, RateMonitor {
+	@property real total_rate() { return mytotal_rate; }
+
+	/// Add an event to the eventlist
+	void add_event( BaseEvent event ) {
+		mytotal_rate = mytotal_rate + event.rate;
+		event.monitor = this;
+		container ~= [event];
+	}
+
+	unittest {
+		auto el = new EventList();
+		assert( el.total_rate == 0 );
+		auto ev1 = new BaseEvent();
+		ev1.rate = 3.0;
+		el.add_event( ev1 );
+		assert( el.total_rate == 3.0 );
+		ev1.rate = 4.0;
+		assert( el.total_rate == 4.0 );
+	}
+
+	void update_rate( real old_rate, real new_rate ) {
+		mytotal_rate = mytotal_rate + (new_rate - old_rate);
+	}
+
+	/// Get the next event
+	BaseEvent get_next_event( ref Random gen ) {
+		real rnd = uniform!("()")( 0, total_rate, gen );
+		real sum = 0;
+		if (rnd < 0.5*total_rate) { 
+			foreach ( ev; container ) {
+				sum = sum + ev.rate;
+				if (sum > rnd)
+					return ev;
+			}
+		} else { // rnd so big, better start looking at the end
+			rnd = total_rate - rnd;
+			foreach_reverse ( ev; container ) {
+				sum = sum + ev.rate;
+				if (sum > rnd)
+					return ev;
+			}
+		}
+		return null;
+	}
+
+	unittest {
+		import std.stdio;
+		Random gen;
+		auto el = new EventList();
+		auto ev1 = new BaseEvent();
+		ev1.rate = 10.0;
+		el.add_event( ev1 );
+		assert( el.get_next_event( gen ) == ev1 );
+
+		auto ev2 = new BaseEvent();
+		ev2.rate = 90.0;
+		el.add_event( ev2 );
+		size_t ev2_count = 0;
+		real t = 0;
+		foreach ( i; 1..1000 ) {
+			t += el.time_till_next_event( gen );
+			if (el.get_next_event( gen ) == ev2)
+				ev2_count++;
+		}
+		assert( ev2_count > 890 && ev2_count < 910 );
+		assert( t > 9 && t < 11 );
+	}
+
+
+	private:
+		real mytotal_rate = 0;
+		auto container = DList!BaseEvent();
 }
