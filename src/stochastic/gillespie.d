7cf6c3c43f0b8164904bc50ec7f07f7441d29e31
BlackEdder <edwin@tkwsping.nl>
2014-05-23 09:03:40 +0100

    Move unittest outside of class definition
    Needed since class is template class and will not otherwise be compiled

--- src/stochastic/gillespie.d
+++ src/stochastic/gillespie.d
@@ -51,59 +51,6 @@ final class Gillespie( T ) {
 				})( initState );
 	}
 
-	/// Birth Death example
-	unittest {
-		size_t deathCount = 0;
-		void death(T)( Gillespie!(T) population, ref size_t density, EventId birthId,
-				EventId deathId ) {
-			deathCount++;
-			population.delEvent( birthId );
-			population.delEvent( deathId );
-			density -= 1;
-		}
-
-		size_t birthCount = 0;
-		void birth(T)( Gillespie!(T) population, ref size_t density ) {
-			density += 1;
-			birthCount++;
-			auto birthId = population.newEventId;
-			population.addEvent( birthId, 0.03, 
-					delegate () => birth( population, density ) );
-			auto deathId = population.newEventId;
-			population.addEvent( deathId, 0.02, 
-					delegate () => death( population, density, birthId, deathId ));
-		}
-
-		Random gen;
-
-		auto population = new Gillespie!(void delegate())();
-
-		size_t density = 0;
-
-		foreach ( i; 0..100 ) {
-			birth( population, density ); // Initial population
-		}
-
-		foreach ( state; population.simulation( gen ) ) {
-			if (state[0] > 100) { // Stop after time > 100
-				break;
-			}
-
-			state[1](); // Execute event
-
-			if (density == 0) {
-				break;
-			}
-			assert( population.length == 2*density ); // Birth and death event for each individual
-			real exactRate = density*(0.03 + 0.02);
-			assert( 0.9999*exactRate < population.rate  && population.rate < 1.0001*exactRate  );
-
-	
-		}
-		real ratio = (cast(real) birthCount-100)/deathCount;
-		assert( 0.9*ratio < 0.03/0.02  && 0.03/0.02 < 1.1*ratio  );
-	}
-
 
 	/// Total rate of all events combined
 	@property auto rate() {
@@ -154,34 +101,6 @@ final class Gillespie( T ) {
 		assert( false, "This should not happen" );
 	}
 
-	unittest {
-		import std.stdio;
-		Random gen;
-		auto gillespie = new Gillespie!(void delegate())();
-		auto ev1Id = gillespie.newEventId;
-		size_t callEv1 = 0;
-		auto l1 = () => (callEv1 += 1, write("")); // Write is to force void delegate
-		gillespie.addEvent( ev1Id, 10.0, l1 );
-		gillespie.getNextEvent( gen )();
-		assert( callEv1 == 1 );
-
-		auto ev2Id = gillespie.newEventId;
-		size_t ev2Count = 0;
-		auto l2 = () => (ev2Count += 1, write(""));
-		gillespie.addEvent( ev2Id, 90.0, l2 );
-
-		real t = 0;
-		foreach ( i; 1..1000 ) {
-			t += gillespie.timeTillNextEvent( gen );
-			gillespie.getNextEvent( gen )();
-		}
-		assert( ev2Count > 890 && ev2Count < 911 );
-		assert( t > 9 && t < 11 );
-
-		gillespie.updateRate( ev2Id, 20.0 );
-		assert( gillespie.rate == 30.0 );
-	}
-
 	/// Time till next event
 	real timeTillNextEvent( ref Random gen ) {
 		return stochastic.random.exponential( myRate, gen );
@@ -199,4 +118,88 @@ final class Gillespie( T ) {
 		T[const EventId] events;
 
 		EventId lastId = 0;
+}
+
+version(unittest) {
+	import std.stdio;
+}
+
+/// Birth Death example
+unittest {
+	size_t deathCount = 0;
+	void death(T)( Gillespie!(T) population, ref size_t density, EventId birthId,
+			EventId deathId ) {
+		deathCount++;
+		population.delEvent( birthId );
+		population.delEvent( deathId );
+		density -= 1;
+	}
+
+	size_t birthCount = 0;
+	void birth(T)( Gillespie!(T) population, ref size_t density ) {
+		density += 1;
+		birthCount++;
+		auto birthId = population.newEventId;
+		population.addEvent( birthId, 0.03, 
+				delegate () => birth( population, density ) );
+		auto deathId = population.newEventId;
+		population.addEvent( deathId, 0.02, 
+				delegate () => death( population, density, birthId, deathId ));
+	}
+
+	Random gen;
+
+	auto population = new Gillespie!(void delegate())();
+
+	size_t density = 0;
+
+	foreach ( i; 0..100 ) {
+		birth( population, density ); // Initial population
+	}
+
+	foreach ( state; population.simulation( gen ) ) {
+		if (state[0] > 100) { // Stop after time > 100
+			break;
+		}
+
+		state[1](); // Execute event
+
+		if (density == 0) {
+			break;
+		}
+		assert( population.length == 2*density ); // Birth and death event for each individual
+		real exactRate = density*(0.03 + 0.02);
+		assert( 0.9999*exactRate < population.rate  && population.rate < 1.0001*exactRate  );
+
+
+	}
+	real ratio = (cast(real) birthCount-100)/deathCount;
+	assert( 0.9*ratio < 0.03/0.02  && 0.03/0.02 < 1.1*ratio  );
+}
+
+unittest {
+	Random gen;
+	auto gillespie = new Gillespie!(void delegate())();
+	auto ev1Id = gillespie.newEventId;
+	size_t callEv1 = 0;
+	auto l1 = () => (callEv1 += 1, write("")); // Write is to force void delegate
+	gillespie.addEvent( ev1Id, 10.0, l1 );
+	gillespie.getNextEvent( gen )();
+	assert( callEv1 == 1 );
+
+	auto ev2Id = gillespie.newEventId;
+	size_t ev2Count = 0;
+	auto l2 = () => (ev2Count += 1, write(""));
+	gillespie.addEvent( ev2Id, 90.0, l2 );
+
+	real t = 0;
+	foreach ( i; 1..1000 ) {
+		t += gillespie.timeTillNextEvent( gen );
+		gillespie.getNextEvent( gen )();
+	}
+	assert( ev2Count > 890 && ev2Count < 911 );
+	assert( t > 9 && t < 11 );
+
+	gillespie.updateRate( ev2Id, 20.0 );
+	assert( gillespie.rate == 30.0 );
 }
